8ea33424faef65e886976459a60fb873c83b1ace
unknown <erik@computer.com>
2021-03-05 10:36:49 -0700

    Nacbar: added navbar functionality

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,7 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Profile = (props) => {
-//    const {} = props;
+        const { handleLogout, user } = props;
+        const { id, name, email, exp } = user;
+        const expirationTime = new Date(exp * 1000);
+        let currentTime = Date.now();
+
+        if (currentTime >= expirationTime) {
+            handleLogout();
+            alert('Session has ended. Please log in to continue.');
+        }
+
+        const userData = user ?
+        (<div>
+            <h1>Profile</h1>
+            <p>Name: {name}</p>
+            <p>Email: {email}</p>
+            <p>ID: {id}</p>
+        </div>) :<h2>Loading...</h2>
 
     const errorDiv = () => {
         return (
@@ -13,8 +29,8 @@ const Profile = (props) => {
     };
     
     return (
-        <div>
-            {/* { Add code here } */}
+        <div className="text-center pt-4">
+          { user ? userData : errorDiv() }
         </div>
     );
 
